9a138c43594e744fe7d5e3e92d1b3d7e7432cf04
Raul Alanis <rulosc07@gmail.com>
2017-02-09 13:53:18 -0600

    disenio de vistas peticionPass y cambiarPass

--- modules/ModUsuarios/views/manager/login.php
+++ modules/ModUsuarios/views/manager/login.php
@@ -24,37 +24,37 @@ $this->registerCssFile ( '@web/css/register-v3.css', [
 		
 			<div class="panel-body">
 
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+				<div class="site-login">
+				    <h1><?= Html::encode($this->title) ?></h1>
+				
+				
+				    <?php $form = ActiveForm::begin(); ?>
+						
+						<div class="form-group form-material floating">
+				    	    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+						</div>
+				
+						<div class="form-group form-material floating">
+					        <?= $form->field($model, 'password')->passwordInput() ?>
+						</div>
+				
+				        <?php $form->field($model, 'rememberMe')->checkbox([
+				            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+				        ]) ?>
+				
+				        <div class="form-group">
+				               <?= Html::submitButton('Ingresar', ['class' => 'btn btn-primary btn-block btn-lg margin-top-40', 'name' => 'login-button']) ?>
+				        </div>
+				
+				    <?php ActiveForm::end(); ?>
+				    
+				</div>
+				
+				<?=Html::a('Reenviar correo de activación', ['reenviar-activacion'])?>
+				<br>
+				<?=Html::a('Olvide mi contraseña', ['peticion-pass'])?>
 
-
-    <?php $form = ActiveForm::begin(); ?>
-		
-		<div class="form-group form-material floating">
-    	    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-		</div>
-
-		<div class="form-group form-material floating">
-	        <?= $form->field($model, 'password')->passwordInput() ?>
-		</div>
-
-        <?php $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
-
-        <div class="form-group">
-               <?= Html::submitButton('Ingresar', ['class' => 'btn btn-primary btn-block btn-lg margin-top-40', 'name' => 'login-button']) ?>
-        </div>
-
-    <?php ActiveForm::end(); ?>
-    
-</div>
-
-<?=Html::a('Reenviar correo de activación', ['reenviar-activacion'])?>
-<br>
-<?=Html::a('Olvide mi contraseña', ['peticion-pass'])?>
-
-</div>
+			</div>
 		
 		</div>
 		
